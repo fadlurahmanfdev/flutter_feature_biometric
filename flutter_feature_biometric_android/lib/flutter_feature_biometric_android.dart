cb4521baced9d33cb137c9da67a680c35b2ef53e
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-09-22 10:18:08 +0700

    setup flutter feature biometric android

--- flutter_feature_biometric_android/lib/flutter_feature_biometric_android.dart
+++ flutter_feature_biometric_android/lib/flutter_feature_biometric_android.dart
@@ -1,13 +1,8 @@
 
-import 'package:flutter_feature_biometric_android/src/messages.g.dart';
-import 'package:flutter_feature_biometric_platform_interface/flutter_feature_biometric_platform_interface.dart';
+import 'flutter_feature_biometric_android_platform_interface.dart';
 
-class FlutterFeatureBiometricAndroid extends FlutterFeatureBiometricPlatform {
-
-  final _api = FlutterFeatureBiometricApi();
-
-  @override
-  Future<bool> isDeviceSupportBiometric() {
-    return _api.isDeviceSupported();
+class FlutterFeatureBiometricAndroid {
+  Future<String?> getPlatformVersion() {
+    return FlutterFeatureBiometricAndroidPlatform.instance.getPlatformVersion();
   }
 }
