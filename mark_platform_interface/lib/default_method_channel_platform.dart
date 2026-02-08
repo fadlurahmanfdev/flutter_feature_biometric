4583808e6af3fca0f7c38756a16e443f35431ca7
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2026-02-08 13:19:49 +0700

    feat(refactor-library): fixing dart analyze result

--- mark_platform_interface/lib/default_method_channel_platform.dart
+++ mark_platform_interface/lib/default_method_channel_platform.dart
@@ -2,10 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/services.dart';
 import 'mark_platform_interface.dart';
-
-const MethodChannel _channel = MethodChannel('plugins.flutter.io/mark');
 
 /// The default interface implementation acting as a placeholder for
 /// the native implementation to be set.
