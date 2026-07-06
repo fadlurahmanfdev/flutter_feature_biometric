646baa9a83738d7d10c1b8fe4598dc8c775b59d3
Taufik Fadlurahman Fajari <taufikfadlurahmanfajari.dev@gmail.com>
2026-07-06 20:16:41 +0700

    feat(library): initial refactor

--- mark_biometric/lib/mark_biometric.dart
+++ mark_biometric/lib/mark_biometric.dart
@@ -1,4 +1,2 @@
 export 'src/mark_biometric.dart';
-export 'package:mark_android/mark_android.dart';
-export 'package:mark_ios/mark_ios.dart';
 export 'package:mark_platform_interface/mark_platform_interface.dart';
